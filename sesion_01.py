cfa0e4ae82f3435be87eb7992c8039cb38a81cdd
Jade-hm <evelynjade09@gmail.com>
2026-02-12 18:41:12 -0800

    guardando mu sesion de python basico

---
+++ sesion_01.py
@@ -0,0 +1,27 @@
+# Numerico y flotante 
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7))
+print(int(1+2))
+print(int(10+2))
+print("==== Operadores Matematicos ====")
+# +
+# -
+# *
+# /
+# ==
+# % Modulo 
+edad = 16
+if (edad >= 18):
+    print("Sipuedo entrar al bar!")
+else:
+    print("No puedo entrar al bar!")
+
+mi_numero = int(input("¿Cual es el numero que deseas verificar?: "))
+print(f"El numero que deseas verificar es {mi_numero}")
+if mi_numero % 2 == 0:
+    print(f"El numero{mi_numero} es par!!!")
+else: 
+    print(f"El numero {mi_numero} es impar!!!")
+    
